29e4c20442d25c7a84ff8d9d49746fb682683d6f
Haoyu Li <haoyu.li@columbia.edu>
2021-03-08 11:01:47 -0700

    Update Mikefile

--- build_version.cc
+++ build_version.cc
@@ -1,5 +1,5 @@
 // Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@ae353b89dfd04e49178d7da496728af49d0ef846@";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2021/02/27 04:34:51@";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@858866f73d359a6573eff0a3bddb7fb9c7894528@";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2021/03/04 16:08:33@";
 const char* rocksdb_build_compile_date = __DATE__;
